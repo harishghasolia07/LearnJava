59efd9e55070f973a3df82805257be49aba69b30
Harish Ghasolia <harishghasolia@gmail.com>
2024-02-16 00:01:03 +0530

    Day 2

---
+++ src/Java_String_14.java
@@ -0,0 +1,17 @@
+public class Java_String_14 {
+    public static void main(String[] args){
+        String name = "Harish";
+        int length = name.length();
+        String lString = name.toLowerCase();
+        System.out.println(lString);
+        String nonTrimmedString = "   Harish   ";
+        String Trimming = nonTrimmedString.trim();
+        System.out.println(Trimming);
+        System.out.println(name.substring(2));
+        System.out.println(name.substring(2,4));
+        System.out.println(name.replace('r','i'));
+        System.out.println(name.replace("ri", "ll"));
+        System.out.println(name.startsWith("Hari"));
+
+    }
+}
---
+++ src/Java_String_Practice_15.java
@@ -0,0 +1,15 @@
+public class Java_String_Practice_15 {
+    public static void main(String[] args){
+        String Text = "Say My Name";
+        System.out.println(Text.replace(" ","_"));
+        String letter = "Dear <|name|> thanks a lot!!";
+        letter = letter.replace("<|name|>","Harish");
+        System.out.println(letter);
+        String newString = "This string contains  double   & triple spaces";
+        System.out.println(newString.indexOf(" "));
+        System.out.println(newString.indexOf("  "));
+        System.out.println(newString.indexOf("   "));
+        System.out.println(newString.indexOf("    "));
+
+    }
+}
