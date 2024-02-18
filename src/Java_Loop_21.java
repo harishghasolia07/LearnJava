2d0a836e0754f18f4d2bf6b7e8f92736444794de
Harish Ghasolia <harishghasolia@gmail.com>
2024-02-18 12:18:54 +0530

    Day-5

---
+++ src/Java_Loop_21.java
@@ -0,0 +1,21 @@
+public class Java_Loop_21 {
+    public static void main(String[] args) {
+//        int i=1;
+//        while(i<10){
+//            System.out.println("Hello");
+//            i=i+2;
+//        }
+//        do{
+//            System.out.println(i);
+//            i++;
+//        }while (i<6);
+        for (int i=5;i>0;i--){
+            if(i==2){
+                System.out.println("You'r Great!!");
+                continue;
+            }
+            System.out.println(i);
+            System.out.println("Java is Great!!z");
+        }
+    }
+}
---
+++ src/Java_Practice_25.java
@@ -0,0 +1,86 @@
+public class Java_Practice_25 {
+    public static void main(String[] args) {
+//        Q.1--------------------------------------
+
+//        for (int i = 5; i>0; i--) {
+//            for (int j = 0; j < i; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.print("\n");
+//
+//        }
+//        Q.2--------------------------------------
+//
+//        int sum=0;
+//        int i=0;
+//        while(i<3){
+//            sum = sum+(2*i);
+//            i++;
+//        }
+//        By For Loop
+//        System.out.println(sum);
+//        int sum1 = 0;
+//        int m=3;
+//        for(int j=0; j<m; j++){
+//            sum1 = sum1 + (2*j);
+//        }
+//        System.out.println(sum1);
+//        Q.3--------------------------------------
+//          int n=5;
+//          for (int i = 1; i < 11; i++) {
+//              System.out.printf("%d X %d = %d\n",n,i,n*i);
+//        }
+//        Q.4------------------------------------
+//          int n=10;
+//          for (int i = 10; i>0; i--) {
+//              System.out.printf("%d X %d = %d\n",n,i,n*i);
+//        }
+//        Q.5------------------------------------
+//          n = n x n-1 x n-2 ...1
+//          5 = 5x4x3x2x1
+//            int n =5;
+//            int i=1;
+//            int fact=1;
+//            while (i<=n){
+//                fact = fact*i;
+//                fact *= i;
+//                i++;
+//            }
+//        System.out.print(fact);
+//
+//            By For loop -->
+//        int n=5;
+//        int fact = 1;
+//        for (int i = 1; i <= n; i++) {
+//            fact *= i;
+//        }
+//        System.out.print(fact);
+//
+//        Q.6--------------------------------
+//        int i=5;
+//        while(i>0){
+//            int j=i;
+//            while (j>0){
+//                System.out.print("*");
+//                j--;
+//            }
+//            System.out.print("\n");
+//            i--;
+//        }
+//
+//        Q.7 ------------------------------------
+//        int n = 8;
+//        int sum = 0;
+//        for (int i = 1; i < 11; i++) {
+//            System.out.println(n*i);
+//            sum += n*i;
+//        }
+//        System.out.print("Sum is : "+sum);
+//
+//        Q.8 ------------------------------------
+
+
+
+
+    }
+    }
