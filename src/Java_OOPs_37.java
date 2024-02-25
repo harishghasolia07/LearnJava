b1b6091377d640be28675dcc233ed195fb38bbff
Harish Ghasolia <harishghasolia@gmail.com>
2024-02-25 13:35:01 +0530

    Day-11

--- src/Java_OOPs_37.java
+++ src/Java_OOPs_37.java
@@ -1,6 +1,21 @@
+class employee{
+    int id;
+    String name;
+    public void printdetails(){
+        System.out.println(name);
+        System.out.println(id);
+    }
+}
+
 public class Java_OOPs_37 {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
+        employee boy = new employee();
+        employee boy1 = new employee();
+        boy.name = "Harish";
+        boy.id = 7;
+        boy1.name = "Harry";
+        boy1.id = 4;
+        boy.printdetails();
+        boy1.printdetails();
     }
 }
